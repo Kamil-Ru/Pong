6c5fda9385d3a4549ec7250a5a6171a16b25cb97
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-05-13 17:34:30 +0200

    Making change class "Paddle"

--- paddle.py
+++ paddle.py
@@ -2,27 +2,23 @@ from turtle import Turtle
 MOVE_PADDLE = 20
 
 
-class Paddle:
+class Paddle(Turtle):
 
     def __init__(self, x_cor=350, y_cor=0):
+        super().__init__()
         self.x_cor = x_cor
         self.y_cor = y_cor
-        self.turtle = self.turtle()
-
-    def turtle(self):
-        turtle = Turtle()
-        turtle.goto(self.x_cor, self.y_cor)
-        turtle.shape("square")
-        turtle.color("white")
-        turtle.shapesize(stretch_wid=5, stretch_len=1)
-        turtle.penup()
-        return turtle
+        self.goto(self.x_cor, self.y_cor)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
 
     def move_up(self):
         self.y_cor += MOVE_PADDLE
-        self.turtle.goto(self.x_cor, self.y_cor)
+        self.goto(self.x_cor, self.y_cor)
 
     def move_down(self):
         self.y_cor -= MOVE_PADDLE
-        self.turtle.goto(self.x_cor, self.y_cor)
+        self.goto(self.x_cor, self.y_cor)
 
